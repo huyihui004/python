eb461e83e30cdf008d8f2d35db39ba07700a2980
huyihui <huyihui004@163.com>
2018-01-29 16:48:24 +0800

    update chatianqi.py

--- 自己/公司电脑py文件/chatianqi.py
+++ 自己/公司电脑py文件/chatianqi.py
@@ -4,13 +4,20 @@
 import urllib2
 import re
 import city
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
-City=raw_input(u'请输入城市：')
-cityid=city.city[City]
+City=raw_input(u"请输入城市:")
+try:
+    cityid=city.city[City]
+except:
+    print('城市不存在！')
+    exit()
 URL="http://www.weather.com.cn/data/cityinfo/%s.html" %(cityid)
 urlopen=urllib2.urlopen(URL)
 urlhtml=urlopen.read()
 reg=re.compile('"cityid":".*?","temp1":"(.*?)","temp2":"(.*?)","weather":"(.*?)"',re.S)
 Weather=re.findall(reg,urlhtml)
 w=list(Weather[0])
-print '%s城市天气预报：%s~%s,%s' %(City,w[0],w[1],w[2])
+print '天气预报：%s~%s,%s' %(w[0],w[1],w[2])
